61c2f6f5cad593b8d6ac48d2236cfe2dffa9ef46
Chris Combs <ccombs@gmail.com>
2024-12-02 20:10:45 -0500

    day 2

---
+++ 02/02-2.py
@@ -0,0 +1,55 @@
+
+lefts = []
+rights = [] 
+
+def validate(vals):
+    
+    bads = 0
+    direction = 0 
+    og_vals = vals.copy()
+    left = vals[0]
+    
+    for right in vals[1:]:
+        diff = right - left
+        if diff < -3 or diff > 3:
+            print(left, "to", right, "delta", diff, "too extreme")
+            bads += 1
+            continue            
+        
+        if diff == 0:
+            print("no change", left, right)
+            bads += 1
+            continue
+            
+        if direction == 0:
+            direction = diff
+            print(left, "to", right, "direction is now", direction)
+            
+        else:
+            if direction < 0:
+                if diff > 0:
+                    print(left, "to", right, "reversed direction", diff, direction)
+                    bads += 1
+                    continue
+            else:
+                if diff < 0:
+                    print(left, "to", right, "reversed direction", diff, direction)
+                    bads += 1
+                    continue
+            
+        left = right
+    print(bads, "bads for", og_vals)
+    return 1 * (bads <= 1)
+    
+score = 0
+
+with open("input", "r") as fh:
+    for line in fh.readlines():
+        vals = [ int(v) for v in line.split() ] 
+        
+        goodly = validate(vals) + validate(list(reversed(vals)))
+
+        if goodly:
+            score += 1
+        
+print(score)
